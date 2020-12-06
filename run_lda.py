49ab5426533dffacced652a8c160dbefd7756ee8
Mihir Yerande <51039100+mihiryerande@users.noreply.github.com>
2020-12-05 21:14:44 -0500

    Helper package for notebooks

---
+++ run_lda.py
@@ -0,0 +1,75 @@
+from gensim.models import CoherenceModel, LdaModel
+import json
+from numpy.random import RandomState
+
+
+def read_lda_input(filename):
+    """
+    Read LDA input text from a specified file.
+
+    Args:
+        filename (str): A *.jl filename from which to read the LDA input text
+
+    Returns:
+        list[list[str]]: List of texts, where a text is a list of tokens (strs)
+    """
+    with open(filename) as f:
+        lda_input = []
+        for line in f:
+            # Extract scraped JSON data for single anime show
+            json_line = json.loads(line)
+            text = json_line['LDA text']
+            toks = text.split()
+            lda_input.append(toks)
+    return lda_input
+
+
+def get_lda_model(corpus, id2word, k, r, alpha=None, eta=None):
+    """
+    Run LDA with some specified inputs.
+
+    Args:
+        corpus (list[list[(int,int)]]): Bag-of-words representation of texts
+        id2word (gensim.corpora.dictionary.Dictionary): A gensim Dictionary
+        k (int): Number of topics
+        r (int): Integer to seed random starting state, for reproducibility
+        alpha ({float, str}): Optional hyperparameter for LDA
+        eta ({float,str}): Optional hyperparameter for LDA
+
+    Returns:
+        gensim.models.LdaModel: LDA output model object
+    """
+    lda_model = LdaModel(
+        corpus=corpus,
+        num_topics=k,
+        id2word=id2word,
+        chunksize=100,
+        random_state=RandomState(r),
+        update_every=1,
+        alpha=(alpha or 'symmetric'),
+        eta=(eta or None),
+        per_word_topics=True
+    )
+    return lda_model
+
+
+def compute_coherence(lda_model, texts, corpus, id2word):
+    """
+    Compute the Coherence score for a given LDA model.
+
+    Args:
+        lda_model (gensim.models.LdaModel): LDA model object from gensim
+        texts (list[list[str]]): List of texts, where a text is a list of tokens (strs)
+        corpus (list[list[(int,int)]]): Bag-of-words representation of texts
+        id2word (gensim.corpora.dictionary.Dictionary): A gensim Dictionary
+
+    Returns:
+        float: Coherence score of the LDA model
+    """
+    coherence_model = CoherenceModel(
+        model=lda_model,
+        texts=texts,
+        corpus=corpus,
+        dictionary=id2word
+    )
+    return coherence_model.get_coherence()
