003f9599bb4b7d3fb4306825aca3a47b64dec2e2
Mihir Yerande <51039100+mihiryerande@users.noreply.github.com>
2020-12-05 22:20:42 -0500

    Upload results from LDA

---
+++ lda_output/genre_names.jl
@@ -0,0 +1,4 @@
+{"LDA Genre ID": 0, "LDA Genre Name": "Potato"}
+{"LDA Genre ID": 1, "LDA Genre Name": "Lettuce"}
+{"LDA Genre ID": 2, "LDA Genre Name": "Cabbage"}
+{"LDA Genre ID": 3, "LDA Genre Name": "Broccoli"}
