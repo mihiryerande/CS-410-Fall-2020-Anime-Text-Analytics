1a05302ae62180342e9fa805e2f8ec6dda9e80ad
Mihir Yerande <51039100+mihiryerande@users.noreply.github.com>
2020-12-13 00:06:20 -0500

    Write LDA model output as JSONs

---
+++ source_code/lda_output/genre_names.jl
@@ -0,0 +1,6 @@
+{"LDA Genre ID": 0, "LDA Genre Name": "Adventure"}
+{"LDA Genre ID": 1, "LDA Genre Name": "Sports"}
+{"LDA Genre ID": 2, "LDA Genre Name": "Sci-Fi"}
+{"LDA Genre ID": 3, "LDA Genre Name": "Mystery"}
+{"LDA Genre ID": 4, "LDA Genre Name": "Slice of Life"}
+{"LDA Genre ID": 5, "LDA Genre Name": "School"}
